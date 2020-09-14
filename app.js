33e46e78de4d5972946e2c6a88fc639665a61cd4
mohamadsamie <mohamad.samieshad@gmail.com>
2020-09-14 13:23:15 +0430

    directive, v-once

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 new Vue({
-    el: '#my-app',
+    el: '#my-app1',
     data: {
-        title: 'هر چی بنویسی اینجا هم نشون میده :)'
+        title: 'هر چی بنویسی اینجا هم نشون میده :)',
+        link: 'https://google.com'
     },
     methods: {
         changeTitle: function (event) {
             this.title = event.target.value
+        },
+        // غییر دادن متن همزمان با نوشتن
+    }
+})
+
+new Vue({
+    el: '#my-app2',
+    data: {
+        message: 'پیام اولیه :)',
+    },
+    methods: {
+        sayHello: function(){
+            this.message = "سلام دوست من :)"
+            return this.message
         }
     }
 })
